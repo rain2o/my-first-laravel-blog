8f8a83c950c5dfb7d2830771de4a7b1354d94348
Joel Rainwater <Joel@Rain2oDesigns.com>
2014-12-06 00:35:26 -0500

    Initial push

---
+++ laravation-master/Gruntfile.js
@@ -0,0 +1,48 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    sass: {
+      options: {
+        includePaths: ['public/assets/vendor/foundation/scss']
+      },
+      dist: {
+        options: {
+          outputStyle: 'compressed'
+        },
+        files: {
+          'public/assets/css/app.css': 'public/assets/scss/app.scss'
+        }
+      }
+    },
+
+    compass: {
+      dist: {
+        options: {
+          importPath: 'public/assets/vendor/foundation/scss',
+          sassDir: 'public/assets/scss',
+          cssDir: 'public/assets/css',
+          environment: 'production',
+          outputStyle: 'compressed'
+        }
+      }
+    },
+
+    watch: {
+      grunt: { files: ['Gruntfile.js'] },
+
+      sass: {
+        files: 'public/assets/scss/**/*.scss',
+        tasks: ['sass']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  //grunt.registerTask('build', ['sass']);
+  grunt.registerTask('build', ['compass']);
+  grunt.registerTask('default', ['build','watch']);
+}
